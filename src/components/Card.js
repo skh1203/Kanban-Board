c31fa872730ba9f72dbbbcc9a2c4811ea7049bf5
Akshay Waghmare <120705327+akshayw1@users.noreply.github.com>
2023-09-24 15:00:06 +0530

    Icon-Fixed-LocalSt

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../css/card.css";
+import {
+  FaExclamationCircle,
+  FaFlag,
+  FaStar,
+  FaThumbsUp,
+  FaCheck,
+} from "react-icons/fa";
 // import { FaSliders } from "react-icons/fa";
-export default function Card({ id, title, tags, priority, groupingType, checked }) {
-  const [check, setCheck]=useState(checked);
-	const handleCheckboxChange = (event,id) => {
-		setCheck((prevValue) => !prevValue);
-		document.getElementById(id).checked = !checked;
-		let checkedId = JSON.parse(localStorage.getItem("checkedID"));
-		console.log(typeof checkedId);
-		if(checkedId.includes(id)){
-			
-			const newArray = checkedId.filter(i => i !== id);
-			localStorage.setItem("checkedID", JSON.stringify(newArray));	
-		}
-		else{
-			checkedId.push(id);
-			localStorage.setItem("checkedID", JSON.stringify(checkedId));
-		}
-		console.log(checkedId);
-	  };
+export default function Card({
+  id,
+  title,
+  tags,
+  priority,
+  groupingType,
+  checked,
+}) {
+  const [check, setCheck] = useState(checked);
+  const handleCheckboxChange = (event, id) => {
+    setCheck((prevValue) => !prevValue);
+    document.getElementById(id).checked = !checked;
+    let checkedId = JSON.parse(localStorage.getItem("checkedID"));
+    console.log(typeof checkedId);
+    if (checkedId.includes(id)) {
+      const newArray = checkedId.filter((i) => i !== id);
+      localStorage.setItem("checkedID", JSON.stringify(newArray));
+    } else {
+      checkedId.push(id);
+      localStorage.setItem("checkedID", JSON.stringify(checkedId));
+    }
+    console.log(checkedId);
+  };
+
+  const priorityIcons = {
+    0: <FaExclamationCircle />,
+    1: <FaFlag />,
+    2: <FaStar />,
+    3: <FaThumbsUp />,
+    4: <FaCheck />,
+  };
+
   return (
     <>
       <div className="card_container">
@@ -29,14 +50,14 @@ export default function Card({ id, title, tags, priority, groupingType, checked 
         <div className="card_body">
           <div className="card_status">
             <div className="checkbox-wrapper-15">
-            <input
-								className="inp-cbx"
-								id={id}
-								type="checkbox"
-								style={{ display: "none" }}
-								checked={check}
-								onChange={(e)=> handleCheckboxChange(e,id)}
-							/>
+              <input
+                className="inp-cbx"
+                id={id}
+                type="checkbox"
+                style={{ display: "none" }}
+                checked={check}
+                onChange={(e) => handleCheckboxChange(e, id)}
+              />
               <label className="cbx" for={id}>
                 <span>
                   <svg width="12px" height="9px" viewbox="0 0 12 9">
@@ -49,7 +70,7 @@ export default function Card({ id, title, tags, priority, groupingType, checked 
           </div>
         </div>
         <div className="card_footer">
-          <div className="card_priority">{priority}</div>
+          <div className="card_priority">{priorityIcons[priority]}</div>
           <div className="card_feature">
             <div className="card_feature_pre"></div>
             <div className="card_feature_title">Feature Request</div>
--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,18 +1,55 @@
 import React from "react";
 import "../css/sectionHeader.css";
+import {
+  FaListAlt,
+  FaTasks,
+  FaCog,
+  FaCheckCircle,
+  FaBan,
+  FaRegClock, // Icon for "No Priority"
+  FaRegThumbsDown, // Icon for "Low"
+  FaRegThumbsUp, // Icon for "Medium"
+  FaRegStar, // Icon for "High"
+  FaExclamationCircle, // Icon for "Urgent"
+} from "react-icons/fa";
+
+const SECTION_ICONS = {
+  Backlog: <FaListAlt />,
+  Todo: <FaTasks />,
+  "In progress": <FaCog />,
+  Done: <FaCheckCircle />,
+  Canceled: <FaBan />,
+};
+
+const PRIORITY_ICONS = {
+  "No Priority": <FaRegClock />,
+  Low: <FaRegThumbsDown />,
+  Medium: <FaRegThumbsUp />,
+  High: <FaRegStar />,
+  Urgent: <FaExclamationCircle />,
+};
 
 function SectionHeader({ heading, count, onAddCard, onEditSection, grouping }) {
-  if(grouping==="user"){
+  const sectionIcon = SECTION_ICONS[heading];
+  const priorityIcon = PRIORITY_ICONS[heading];
+
+  if (grouping === "user") {
     let nameArr = heading.split(" ");
     nameArr.push(".");
     let randomColors = ["f2352e", "5ff22e", "ebf22e", "2e6cf2", "f22e90"];
     const randomIndex = Math.floor(Math.random() * randomColors.length);
-    const color = randomColors[randomIndex]
-    let url = "https://ui-avatars.com/api/?name="+nameArr[0]+"+"+nameArr[1]+"&background="+color;
+    const color = randomColors[randomIndex];
+    let url =
+      "https://ui-avatars.com/api/?name=" +
+      nameArr[0] +
+      "+" +
+      nameArr[1] +
+      "&background=" +
+      color;
     return (
       <div className="flex justify-between">
         <div className="flex justify-start">
-          <div >
+          <div>
             <img className="section-img" src={url}></img>
           </div>
           <div className="section-heading">{heading}</div>
@@ -28,7 +65,7 @@ function SectionHeader({ heading, count, onAddCard, onEditSection, grouping }) {
   return (
     <div className="flex justify-between">
       <div className="flex justify-start">
-        <div className="section-icon"></div>
+        <div className="section-icon"> {sectionIcon || priorityIcon}</div>
         <div className="section-heading">{heading}</div>
         <div className="section-count">{count}</div>
       </div>
